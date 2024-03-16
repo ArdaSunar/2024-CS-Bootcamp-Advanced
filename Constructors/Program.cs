61e2b14610cf06f98d9d7bb787071f63ff3bec38
ArdaSunar <113684339+ArdaSunar@users.noreply.github.com>
2024-03-16 18:00:44 +0300

    First Commit

---
+++ Constructors/Program.cs
@@ -0,0 +1,33 @@
+﻿Customer customer1 = new Customer() { Id = 1 , FirstName= "Arda", LastName = "Sunar", City = "Samsun"};
+Customer customer2 = new Customer(2, "Abuzer", "Sunar", "Kırıkkale");
+
+Console.WriteLine(customer1.FirstName);
+Console.WriteLine(customer2.FirstName);
+
+
+
+
+
+
+
+
+class Customer
+{
+    public Customer()
+    {
+
+    }
+    public Customer(int id, string firstName, string lastName, string city)
+    {
+        Id = id;
+        FirstName = firstName;
+        LastName = lastName;
+        City = city;
+
+    }
+
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string City { get; set; }
+}
---
+++ Interfaces/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+IPersonManager customer1 = new CustomerManager();
+IPersonManager employee1 = new EmployeeManager();
+
+
+ProjectManager projectManager = new ProjectManager();
+projectManager.Add(customer1);
+projectManager.Add(employee1);
+
+
+
+
+
+
+
+
+interface IPersonManager
+{
+    //unimplemented operation
+    void Add();
+    void Update();
+}
+
+ 
+
+class CustomerManager : IPersonManager
+{
+    public void Add()
+    {
+        Console.WriteLine("Customer Added");
+    }
+
+    public void Update()
+    {
+        Console.WriteLine("Customer Updated");
+    }
+}
+
+class EmployeeManager : IPersonManager
+{
+    public void Add()
+    {
+        Console.WriteLine("Employee Added");
+    }
+
+    public void Update()
+    {
+        Console.WriteLine("Employee Updated");
+    }
+}
+
+class ProjectManager
+{
+    public void Add(IPersonManager personManager)
+    {
+        personManager.Add();
+    }
+}
